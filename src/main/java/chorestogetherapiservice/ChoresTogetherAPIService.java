f26d171be94704e688316b601b69d78144121b66
mikesukim <mikesungunkim@gmail.com>
2022-12-05 19:45:14 -0800

    Add Spock

---
+++ src/main/java/chorestogetherapiservice/ChoresTogetherAPIService.java
@@ -0,0 +1,11 @@
+package chorestogetherapiservice;
+
+public class ChoresTogetherAPIService {
+    public static void main(String[] args) {
+        ChoresTogetherAPIService choresTogetherAPIService = new ChoresTogetherAPIService();
+        System.out.print(choresTogetherAPIService.sayHello());
+    }
+    public String sayHello(){
+        return "SayHello";
+    }
+}
