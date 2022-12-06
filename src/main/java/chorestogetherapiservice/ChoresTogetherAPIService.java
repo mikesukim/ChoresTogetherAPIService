45ad61fcc4191d04e58e84fbbe08cd010cb4f472
mikesukim <mikesungunkim@gmail.com>
2022-12-05 19:51:14 -0800

    Add Logging, sjf4j & log4j2

--- src/main/java/chorestogetherapiservice/ChoresTogetherAPIService.java
+++ src/main/java/chorestogetherapiservice/ChoresTogetherAPIService.java
@@ -1,9 +1,14 @@
 package chorestogetherapiservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ChoresTogetherAPIService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChoresTogetherAPIService.class);
     public static void main(String[] args) {
         ChoresTogetherAPIService choresTogetherAPIService = new ChoresTogetherAPIService();
         System.out.print(choresTogetherAPIService.sayHello());
+        LOGGER.debug("Logger testing");
     }
     public String sayHello(){
         return "SayHello";
