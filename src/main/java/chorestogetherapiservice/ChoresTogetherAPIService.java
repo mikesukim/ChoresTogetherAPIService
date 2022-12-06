5f4bcb6ac2f7caec5e9ea4e1549b319637287181
mikesukim <mikesungunkim@gmail.com>
2022-12-06 00:17:34 -0800

    Add Jersey-Guice

--- src/main/java/chorestogetherapiservice/ChoresTogetherAPIService.java
+++ src/main/java/chorestogetherapiservice/ChoresTogetherAPIService.java
@@ -1,16 +1,41 @@
 package chorestogetherapiservice;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import io.logz.guice.jersey.JerseyModule;
+import io.logz.guice.jersey.JerseyServer;
+import io.logz.guice.jersey.configuration.JerseyConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChoresTogetherAPIService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ChoresTogetherAPIService.class);
-    public static void main(String[] args) {
-        ChoresTogetherAPIService choresTogetherAPIService = new ChoresTogetherAPIService();
-        System.out.print(choresTogetherAPIService.sayHello());
-        LOGGER.debug("Logger testing");
-    }
-    public String sayHello(){
-        return "SayHello";
+    public static void main(String[] args) throws Exception {
+        JerseyConfiguration configuration = JerseyConfiguration.builder()
+                .addPackage("java.chorestogetherapiservice")
+                .addPort(8080)
+                .build();
+
+        List<Module> modules = new ArrayList<>();
+        modules.add(new JerseyModule(configuration));
+
+        Injector injector = Guice.createInjector(modules);
+        JerseyServer server = injector.getInstance(JerseyServer.class);
+
+        server.start();
+        LOGGER.info("server started");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                server.stop();
+                LOGGER.info("server stopped");
+            } catch (Exception e) {
+                LOGGER.error("exception occurred when stopping down server", e);
+            }
+        }));
     }
 }
